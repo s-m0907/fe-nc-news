bc546c03f002433d2bfa34d13918e6dcb538b5cc
s-m0907 <suzewhaites@gmail.com>
2024-03-14 10:34:09 +0000

    amending comment post, disable button waiting for req

--- src/CommentForm.jsx
+++ src/CommentForm.jsx
@@ -20,19 +20,24 @@ const CommentForm = ({setComments}) => {
     const {loggedInUser} = useContext(UserContext)
     const [input, setInput] = useState('')
     const [postSuccess, setPostSuccess] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false)
 
     const { article_id } = useParams()
 
     const user = loggedInUser.username
 
     const handleInputChange = (event) => {
+        console.log(input)
         setInput(event.target.value)
     }
     const handleSubmit = (event) => {
+        setIsDisabled(true)
         event.preventDefault()
         postComment(user, input, article_id).then((commentFromApi) => {
                 setComments((currComments) => {
                     setPostSuccess(true)
+                    setIsDisabled(false)
+                    setInput('')
                     return [commentFromApi, ...currComments]
                 })        
         }).catch(() => {
@@ -45,7 +50,7 @@ return <>
     <Form onSubmit={handleSubmit}>
             <label htmlFor="comment">Add a comment: </label>
             <textarea rows="5" cols="50" id="comment" value= {input} onChange={handleInputChange} name="comment" required/>
-            <button type="submit">Post Comment</button> {postSuccess ? <p>Comment posted!</p> : <p></p>}
+            <button id="button" type="submit" disabled={isDisabled}>Post Comment</button> {postSuccess ? <p>Comment posted!</p> : <p></p>}
             </Form>
 </>
 }
